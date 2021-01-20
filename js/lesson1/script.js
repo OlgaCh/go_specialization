e384762ab59395ff9b0a0ce9374b7b4b65421b11
Olga Chebotaryova <olga.chebotaryova@gmail.com>
2021-01-20 08:46:02 +0300

    JS. Lesson1

---
+++ js/lesson1/script.js
@@ -0,0 +1,13 @@
+// convert temperature in Celsius to the Fahrenheit
+
+let tC = -20; // temperature this morning in my location
+let tF;
+tF = (9 / 5) * tC + 32;
+alert(`Температура по Цельсию:  ${tC}\nТемпература по Фаренгейту: ${tF}`)
+
+// Variables
+let admin;
+let _name = "Василий";
+
+admin = _name;
+alert(`Имя админа - ${admin}`)
